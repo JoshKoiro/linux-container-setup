0f576a2d50ee607efb94779505ea196f969707c1
JoshKoiro <josh.koiro@gmail.com>
2025-09-18 14:49:43 -0400

    updated env-setup.sh with post-install brew crap

--- env-setup.sh
+++ env-setup.sh
@@ -3,6 +3,12 @@
 # Download and Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Complete installation of homebrew
+echo >> ~/.bashrc
+echo  'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shell env)"
+sudo nala install build-essential
+
 # Download and Install eza
 brew install eza
 
